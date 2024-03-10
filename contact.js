a2041f6322e32ed12337dfc43dac80effe5d193f
sarae5491 <157627146+sarae5491@users.noreply.github.com>
2024-03-10 18:31:03 +0000

    fix links

--- contact.js
+++ contact.js
@@ -2,5 +2,5 @@ document
   .getElementById("contact_form")
   .addEventListener("submit", function (event) {
     event.preventDefault(); // Prevents the default form submission action
-    window.location.href = "/thankyou.html"; // Redirects to the "Thank You" page
+    window.location.href = "thankyou.html"; // Redirects to the "Thank You" page
   });
