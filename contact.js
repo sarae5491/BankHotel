5160ffcb8c174bfd7c75d451a64124dfd5eaf52f
sarae5491 <157627146+sarae5491@users.noreply.github.com>
2024-03-10 17:19:53 +0000

    add image links

--- contact.js
+++
@@ -1,6 +0,0 @@
-document
-  .getElementById("contact_form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevents the default form submission action
-    window.location.href = "/thankyou.html"; // Redirects to the "Thank You" page
-  });
